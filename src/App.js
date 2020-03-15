25b5001bb87cb3fa990ee55fc2558efd0923f231
tariq-rasheed <tariq.rasheed@peeredge.io>
2020-03-16 02:38:52 +0500

    header added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './style-scss.css';
-// import './style-less.css';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-          <span className="myClass">Welcome to React</span>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Header />
     );
   }
 }
